9302a3bd6a1b33610b6b043d41e6d7a50cc3d22f
Casper V. Kristensen <casper@vkristensen.dk>
2026-04-20 13:35:01 +0200

    python cli

---
+++ clank/main.py
@@ -0,0 +1,55 @@
+import argparse
+import subprocess
+from pathlib import Path
+
+
+def cli() -> None:
+    parser = argparse.ArgumentParser(
+        prog="clank",
+    )
+    parser.add_argument("--init", type=str, required=True)
+    parser.add_argument("--empty", type=str, required=True)
+    parser.add_argument("args", nargs="*")
+    args = parser.parse_args()
+    print(args)  # TODO
+    main(args)
+
+
+def main(args: argparse.Namespace) -> None:
+    command = [
+        "podman",
+        "run",
+        "--rm",
+        "-it",
+        # Kinda yolo, but you need at least `--device=/dev/fuse`, and
+        # `--cap-add=SYS_ADMIN,NET_ADMIN,NET_RAW,mknod` to make compose work
+        # inside the container anyway. Claude tried to break out for like half
+        # an hour without success, so it's probably fine.
+        # https://www.redhat.com/en/blog/podman-inside-container,
+        "--privileged",
+        "--security-opt=label=disable",
+        "--security-opt=apparmor=unconfined",
+        "--volume=/proc/sys:/proc/sys:rw",
+        # TODO
+        f"--volume={Path.home()}/.local/share/containers/storage:/var/lib/shared:ro",
+        f"--volume={Path.home()}/.config/git:/root/.config/git:ro",
+        f"--volume={Path.home()}/.config/clank.env:/root/.config/clank.env:ro",
+        # Mount current directory to /host/
+        "--volume=./:/root/host:rw",
+        # https://discourse.nixos.org/t/running-nix-os-containers-directly-from-the-store-with-podman/29220
+        # https://github.com/metaspace/container-nixos/tree/main
+        "--volume=/nix/store:/nix/store:ro",
+        "--mount=type=tmpfs,tmpfs-size=512M,destination=/run",
+        "--mount=type=tmpfs,tmpfs-size=512M,destination=/run/wrappers,suid",
+        "--systemd=always",
+        "--rootfs",
+        f"{args.empty}:O",
+        args.init,
+    ]
+    try:
+        subprocess.run(command, check=True)
+    except subprocess.CalledProcessError as e:
+        # The systemd init process exits with status code 130 when properly
+        # powered off.
+        if e.returncode not in (0, 130):
+            raise
--- flake.nix
+++ flake.nix
@@ -20,67 +20,40 @@
     packages = forAllSystems (system: let
       pkgs = nixpkgs.legacyPackages.${system};
     in {
+      cli = pkgs.python3Packages.buildPythonApplication {
+        pname = "clank";
+        version = "0.0.1";
+        pyproject = true;
+
+        src = ./.;
+
+        build-system = [pkgs.python3Packages.setuptools];
+
+        doCheck = false; # has no tests, of course
+
+        dependencies = [
+          pkgs.podman
+        ];
+
+        makeWrapperArgs = [
+          "--add-flag"
+          "--init=${self.packages.${system}.container.config.system.build.toplevel}/init"
+          "--add-flag"
+          "--empty=${pkgs.emptyDirectory}"
+        ];
+      };
+
       container = nixpkgs.lib.nixosSystem {
         system = system;
         modules = [./container];
       };
-
-      app = let
-        entrypoint = pkgs.writeShellApplication {
-          name = "entrypoint";
-          runtimeInputs = [
-            pkgs.coreutils
-          ];
-          text = ''
-            # Save program arguments then start systemd. The root user's
-            # `programs.bash.loginShellInit` will execute `/run/shell-init`.
-            echo "$@" > /run/shell-init
-            chmod +x /run/shell-init
-            exec ${self.packages.${system}.container.config.system.build.toplevel}/init
-          '';
-        };
-      in
-        pkgs.writeShellApplication {
-          name = "clank";
-          runtimeInputs = [
-            pkgs.podman
-          ];
-          text = pkgs.lib.strings.concatStringsSep " " [
-            "podman run"
-            "--rm"
-            "-it"
-            # Kinda yolo, but you need at least `--device=/dev/fuse`, and
-            # `--cap-add=SYS_ADMIN,NET_ADMIN,NET_RAW,mknod` to make compose work
-            # anyway.
-            # https://www.redhat.com/en/blog/podman-inside-container
-            "--privileged"
-            "--security-opt label=disable"
-            "--security-opt apparmor=unconfined"
-            "--volume /proc/sys:/proc/sys:rw"
-            # TODO
-            "--volume \"$HOME/.local/share/containers/storage\":/var/lib/shared:ro"
-            # Mount current directory to /host/
-            "--volume ./:/root/host:rw"
-            # https://discourse.nixos.org/t/running-nix-os-containers-directly-from-the-store-with-podman/29220
-            # https://github.com/metaspace/container-nixos/tree/main
-            "--volume /nix/store:/nix/store:ro"
-            "--mount=type=tmpfs,tmpfs-size=512M,destination=/run"
-            "--mount=type=tmpfs,tmpfs-size=512M,destination=/run/wrappers,suid"
-            "--systemd=always"
-            "--rootfs ${pkgs.emptyDirectory}:O"
-            "${entrypoint}/bin/entrypoint"
-            "\"$@\""
-          ];
-        };
     });
 
     # `nix run`
-    apps = forAllSystems (system: let
-      pkgs = nixpkgs.legacyPackages.${system};
-    in {
+    apps = forAllSystems (system: {
       default = {
         type = "app";
-        program = pkgs.lib.getExe self.packages.${system}.app;
+        program = "${self.packages.${system}.cli}/bin/clank";
       };
     });
   };
