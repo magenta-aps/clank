2977ede32b500ed81b20a2d63ba14d5cf3a58c0c
Casper V. Kristensen <casper@vkristensen.dk>
2026-04-23 16:09:08 +0200

    fix: don't claude memory

--- container/base/claude.nix
+++ container/base/claude.nix
@@ -16,6 +16,9 @@
     # DISABLE_AUTOUPDATER, DISABLE_BUG_COMMAND,
     # DISABLE_ERROR_REPORTING and DISABLE_TELEMETRY.
     CLAUDE_CODE_DISABLE_NONESSENTIAL_TRAFFIC = "1";
+    # Don't use memory since we're in an ephemeral container.
+    # https://code.claude.com/docs/en/memory
+    CLAUDE_CODE_DISABLE_AUTO_MEMORY = "1";
   };
 
   # https://code.claude.com/docs/en/settings
